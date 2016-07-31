e0312f2e0e937b31c579eb8f33f214e0de30fb43
Diego Lorenzo <diego@diegolorenzo.com>
2016-07-31 16:00:36 -0400

    Update to remove functions file for now and updating default index.

---
+++ public/content/themes/spark/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The main template file
+ *
+ * @package WordPress
+ * @subpackage Spark
+ * @since Spark 1.0
+ */
+
+
+get_header(); ?>
+
+
+<div id="main" role="main">
+  <p>Welcome to Spark!</p>
+</div>
+
+
+<?php 
+get_footer();
